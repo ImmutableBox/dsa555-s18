c1d558dbd295eb5d54a092aabcff71cd94e96dbe
Catherine Leung <cathyatseneca>
2018-07-16 01:10:20 -0400

    a1 testers, a1q3 starter file name fix

--- a1q3.cpp
+++ a1q3.cpp
@@ -1,9 +1,9 @@
-bool candidate(int array[],int& candidate, int sz);
+bool candidate(int array[],int& myCandidate, int sz);
 bool majorityElement(const int arr[], int& majority, int sz);
 
 bool majorityElement(const int arr[], int& majority, int sz){
 	
 }
-bool candidate(int array[],int& candidate, int sz){
+bool candidate(int array[],int& myCandidate, int sz){
 	
 }

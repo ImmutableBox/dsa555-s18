58f0430506bb45bfde706325b03845913c5e9a90
Catherine Leung <cathyatseneca>
2018-07-25 00:53:31 -0400

    a2 starter file

---
+++ table.h
@@ -0,0 +1,244 @@
+#include <string>
+#include <utility>
+using namespace std;
+
+template <class TYPE>
+class Table{
+public:
+	Table(){}
+	virtual bool update(const string& key, const TYPE& value)=0;
+	virtual bool remove(const string& key)=0;
+	virtual bool find(const string& key, TYPE& value)=0;
+	virtual ~Table(){}
+};
+
+template <class TYPE>
+class SimpleTable:public Table<TYPE>{
+
+	struct Record{
+		TYPE data_;
+		string key_;
+		Record(const string& key, const TYPE& data){
+			key_=key;
+			data_=data;
+		}
+
+	};
+
+	Record** records_;   //the table
+	int max_;           //capacity of the array
+	int size_;          //current number of records held
+	int search(const string& key);
+	void sort();
+
+public:
+	SimpleTable(int maxExpected);
+	SimpleTable(const SimpleTable& other);
+	SimpleTable(SimpleTable&& other);
+	virtual bool update(const string& key, const TYPE& value);
+	virtual bool remove(const string& key);
+	virtual bool find(const string& key, TYPE& value);
+	virtual const SimpleTable& operator=(const SimpleTable& other);
+	virtual const SimpleTable& operator=(SimpleTable&& other);
+	virtual ~SimpleTable();
+};
+
+
+//returns index of where key is found.
+template <class TYPE>
+int SimpleTable<TYPE>::search(const string& key){
+	int rc=-1;
+	for(int i=0;i<size_;i++){
+		if(records_[i]->key_==key){
+			rc=i;
+		}
+	}
+	return rc;
+}
+//sort the according to key in table
+template <class TYPE>
+void SimpleTable<TYPE>::sort(){
+   int minIdx=0;
+   for(int i=0;i<size_;i++){
+   		minIdx=i;
+     	for(int j=i+1;j<size_;j++){
+     		if(records_[j]->key_ < records_[minIdx]->key_){
+     			minIdx=j;
+     		}
+    	}
+    	Record* tmp=records_[i];
+    	records_[i]=records_[minIdx];
+    	records_[minIdx]=tmp;
+   }
+}
+
+/* none of the code in the function definitions below are correct.  You can replace what you need
+*/
+template <class TYPE>
+SimpleTable<TYPE>::SimpleTable(int maxExpected): Table<TYPE>(){
+	records_=new Record*[maxExpected];
+	max_=maxExpected;
+	size_=0;
+}
+
+template <class TYPE>
+SimpleTable<TYPE>::SimpleTable(const SimpleTable<TYPE>& other){
+	records_=new Record*[other.max_];
+	max_=other.max_;
+	size_=0;
+	for(int i=0;i<other.size_;i++){
+		update(other.records_[i]->key_,other.records_[i]->data_);
+	}
+}
+template <class TYPE>
+SimpleTable<TYPE>::SimpleTable(SimpleTable<TYPE>&& other){
+	size_=other.size_;
+	max_=other.max_;
+	records_=other.records_;
+	other.records_=nullptr;
+	other.size_=0;
+	other.max_=0;
+}
+
+template <class TYPE>
+bool SimpleTable<TYPE>::update(const string& key, const TYPE& value){
+	int idx=search(key);
+	bool rc=false;
+	if(idx==-1){
+		if(size_ < max_){
+			records_[size_++]=new Record(key,value);
+			sort();
+			rc=true;
+		}
+	}
+	else{
+		rc=true;
+		records_[idx]->data_=value;
+	}
+	return rc;
+}
+
+template <class TYPE>
+bool SimpleTable<TYPE>::remove(const string& key){
+	int idx=search(key);
+	if(idx!=-1){
+		delete records_[idx];
+		for(int i=idx;i<size_-1;i++){
+			records_[i]=records_[i+1];
+		}
+		size_--;
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+template <class TYPE>
+bool SimpleTable<TYPE>::find(const string& key, TYPE& value){
+	int idx=search(key);
+	if(idx==-1)
+		return false;
+	else{
+		value=records_[idx]->data_;
+		return true;
+	}
+}
+
+template <class TYPE>
+const SimpleTable<TYPE>& SimpleTable<TYPE>::operator=(const SimpleTable<TYPE>& other){
+	if(this!=&other){
+		if(records_){
+			int sz=size_;
+			for(int i=0;i<sz;i++){
+				remove(records_[0]->key_);
+			}
+			delete [] records_;
+		}
+		records_=new Record*[other.max_];
+		max_=other.max_;
+		size_=0;
+		for(int i=0;i<other.size_;i++){
+			update(other.records_[i]->key_,other.records_[i]->data_);
+		}
+
+	}
+	return *this;
+}
+template <class TYPE>
+const SimpleTable<TYPE>& SimpleTable<TYPE>::operator=(SimpleTable<TYPE>&& other){
+	swap(records_,other.records_);
+	swap(size_,other.size_);
+	swap(max_,other.max_);
+	return *this;
+}
+template <class TYPE>
+SimpleTable<TYPE>::~SimpleTable(){
+	if(records_){
+		int sz=size_;
+		for(int i=0;i<sz;i++){
+			remove(records_[0]->key_);
+		}
+		delete [] records_;
+	}
+}
+
+template <class TYPE>
+class ChainingTable:public Table<TYPE>{
+public:
+	ChainingTable(int maxExpected);
+	ChainingTable(const ChainingTable& other);
+	ChainingTable(ChainingTable&& other);
+	virtual bool update(const string& key, const TYPE& value);
+	virtual bool remove(const string& key);
+	virtual bool find(const string& key, TYPE& value);
+	virtual const ChainingTable& operator=(const ChainingTable& other);
+	virtual const ChainingTable& operator=(ChainingTable&& other);
+	virtual ~ChainingTable();
+};
+/* none of the code in the function definitions below are correct.  It is there to simply
+allow the testers to compile without warnings/errors.  You can replace what you need
+*/
+template <class TYPE>
+ChainingTable<TYPE>::ChainingTable(int maxExpected): Table<TYPE>(){
+
+}
+
+template <class TYPE>
+ChainingTable<TYPE>::ChainingTable(const ChainingTable<TYPE>& other){
+
+}
+template <class TYPE>
+ChainingTable<TYPE>::ChainingTable(ChainingTable<TYPE>&& other){
+
+}
+template <class TYPE>
+bool ChainingTable<TYPE>::update(const string& key, const TYPE& value){
+	return true;
+}
+
+template <class TYPE>
+bool ChainingTable<TYPE>::remove(const string& key){
+	return true;
+}
+
+template <class TYPE>
+bool ChainingTable<TYPE>::find(const string& key, TYPE& value){
+	return true;
+}
+
+template <class TYPE>
+const ChainingTable<TYPE>& ChainingTable<TYPE>::operator=(const ChainingTable<TYPE>& other){
+	return *this;
+
+}
+template <class TYPE>
+const ChainingTable<TYPE>& ChainingTable<TYPE>::operator=(ChainingTable<TYPE>&& other){
+	return *this;
+
+}
+template <class TYPE>
+ChainingTable<TYPE>::~ChainingTable(){
+
+}
+
